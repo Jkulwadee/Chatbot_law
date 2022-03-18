0f4fd25a893bdea3e12edf1ff9d656e86c032ba4
Sasitorn Sakkaew <creamfridayapril@gmail.com>
2022-03-18 21:52:11 +0700

    change tokens 3/18 , cancel test hello

--- chat_bot/bot/views.py
+++ chat_bot/bot/views.py
@@ -16,12 +16,13 @@ FB_ENDPOINT & PAGE_ACCESS_TOKEN
 Come from the next step.
 """
 FB_ENDPOINT = "https://graph.facebook.com/v12.0/"
-PAGE_ACCESS_TOKEN = "EAACh8LwRraIBAB6wNqmxZC2K2bA55GURfPNWDEFSfQNSUiRa3I49oVgZCxcuun6zTrsw0s4ZCUhXQ33kPb7zN3NSmGZAoJYZBw0ZB762DDnAp3TD9eqVN3yUwcrpyOsBldoFWZAxCmELsSUza8teXsZAYr07VodZAlFWlMttmqWv1DewZDZD"
+PAGE_ACCESS_TOKEN = "EAAeGA2LZAObIBAGOnBgxUIbwWoLR4m43rhkmkF9U1bZAps1ZCnIHlYhpdm5CWPlmQWtZALj9CwoN3ngtmIfqABAk6vofu4O42ktgxct2LRpRkEZBrfq8BWcxh7co0WdaD8rrgZA4Q85BXZCBiv0MduR9rJGC4sriiE1qZAZBeLMUIh3jZBQffd4zxl"
 
 
 def parse_and_send_fb_message(fbid, recevied_message):
     # Remove all punctuations, lower case the text and split it based on space
-    tokens = re.sub(r"[^a-zA-Z0-9\s]", " ", recevied_message).lower().split()
+    # tokens = re.sub(r"[^a-zA-Z0-9\s]", " ", recevied_message).lower().split()
+    tokens = recevied_message.lower().split()
     msg = None
     for token in tokens:
         if token in LOGIC_RESPONSES:
@@ -50,16 +51,17 @@ class FacebookWebhookView(View):
     hub.challenge
     Are all from facebook. We'll discuss soon.
     """
-    # def get(self, request, *args, **kwargs):
-    #     hub_mode   = request.GET.get('hub.mode')
-    #     hub_token = request.GET.get('hub.verify_token')
-    #     hub_challenge = request.GET.get('hub.challenge')
-    #     if hub_token != VERIFY_TOKEN:
-    #         return HttpResponse('Error, invalid token', status_code=403)
-    #     return HttpResponse(hub_challenge)
 
     def get(self, request, *args, **kwargs):
-        return HttpResponse("Hello, World!")
+        hub_mode = request.GET.get("hub.mode")
+        hub_token = request.GET.get("hub.verify_token")
+        hub_challenge = request.GET.get("hub.challenge")
+        if hub_token != VERIFY_TOKEN:
+            return HttpResponse("Error, invalid token", status_code=403)
+        return HttpResponse(hub_challenge)
+
+    # def get(self, request, *args, **kwargs):
+    #     return HttpResponse("Hello, World!")
 
     def post(self, request, *args, **kwargs):
         incoming_message = json.loads(request.body.decode("utf-8"))
